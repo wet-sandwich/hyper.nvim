ff35e4fe763504df34d0a42408ccea6e874bf9c4
Andrew Niles <andrewgniles@gmail.com>
2024-10-11 10:00:58 -0600

    fix use of underscores in headers

--- lua/hyper/utils/table.lua
+++ lua/hyper/utils/table.lua
@@ -10,7 +10,7 @@ end
 
 function M.lines_to_kv(lines, sep)
   local tbl = {}
-  local pattern = "%s*(#?%s*%a[%w%-_]*)%s*" .. sep .. "%s*(.*)"
+  local pattern = "%s*(#?%s*[%w%-_]*)%s*" .. sep .. "%s*(.*)"
   for _, line in ipairs(lines) do
     for k, v in line:gmatch(pattern) do
       tbl[k] = tonumber(v) or v
